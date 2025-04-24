f73e5d5b3bcc116a121ec8a9e03be6af0e16e0d3
AlejoPeque <alejopequeno25@gmail.com>
2025-04-23 21:39:19 -0300

    first commit

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,6 @@
 import { Header } from "@/components/features/header";
 import { MailSidebar } from "@/components/features/mail-sidebar";
-import { Card } from "@/components/ui/card";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 
 export default function Dashboard() {
   return (
@@ -9,8 +9,15 @@ export default function Dashboard() {
       <div className="flex flex-1 overflow-hidden">
         <MailSidebar />
         <main className="flex-1 overflow-y-auto">
-          <div className="p-4 md:pl-0 pt-0">
-            <Card className="h-full"></Card>
+          <div className="p-4 md:pl-0 pt-0 flex h-full">
+            <Card className="flex-1 overflow-y-auto gap-4">
+              <CardHeader className="border-b border-grayColor !pb-4">
+                Hello
+              </CardHeader>
+              <CardContent className="h-full overflow-y-auto p-4 pt-0">
+                <div className="bg-red-500 h-screen w-full"></div>
+              </CardContent>
+            </Card>
           </div>
         </main>
       </div>
